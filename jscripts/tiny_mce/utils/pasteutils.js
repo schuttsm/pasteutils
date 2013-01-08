ee4e0f9a5bd0b5a19990f9385913aa05c993acaa
Stephen Schutt <stephen.schutt@cornerstonenw.com>
2013-01-08 15:24:43 -0800

    refactor & cleanup pasteutils to make it more readable & cleaner.  No functionality or bug fixes.

--- jscripts/tiny_mce/utils/pasteutils.js
+++ jscripts/tiny_mce/utils/pasteutils.js
@@ -16,87 +16,107 @@
  *    editor_plugin_src.js - line 660: function _convertLists, line 338: function _preProcess & line 586 function _postProcess
  *    DOMUtils.js
  */
- 
 (function($) {
 	window.pasteutils = window.pasteutils || {};
+	pasteutils.patterns = {
+		'word_content': /class="?Mso|style="[^"]*\bmso-|w:WordDocument/i,
+		'scrub_A_char': new RegExp(String.fromCharCode(194), 'ig'),
+		'beginning_nbsps': /^\s*(&nbsp;)+/gi,
+		'ending_nbsps': /(&nbsp;|<br[^>]*>)+\s*$/gi,
+		'support_list_marker': '/<!--\[if !supportLists\]-->/gi',
+		'mso_list_marker':  '/(<span[^>]+(?:mso-list:|:\s*symbol)[^>]+>)/gi',
+		'mso_list_symbol_marker': /(<p[^>]+(?:MsoListParagraph)[^>]+>)/gi,
+		'conditional_comments': /<!--[\s\S]+?-->/gi,
+		'other_comments': /<(!|script[^>]*>.*?<\/script(?=[>\s])|\/?(\?xml(:\w+)?|img|meta|link|style|\w:\w+)(?=[\s\/>]))[^>]*>/gi,
+		'ms_strike': /<(\/?)s>/gi,
+		'nbsp_marker': /&nbsp;/gi,
+		'bad_attrs': /(<[a-z][^>]*\s)(?:id|name|language|on\w+|\w+:\w+)=(?:"[^"]*"|\w+)\s?/gi,
+		'span_marker' : /<\/?span[^>]*>/gi,
+		'open_office_scrub': /Version:[\d.]+\nStartHTML:\d+\nEndHTML:\d+\nStartFragment:\d+\nEndFragment:\d+/gi,
+		'ul_match' : /^(__MCE_ITEM__)+[\u2022\u00b7\u00a7\u00d8o\u25CF]\s*\u00a0*/,
+		'ol_match' : /^[__MCE_ITEM__]+\s*\w+(\.|\))\s*\u00a0+/,
+		'i_bullet_type' : /^[__MCE_ITEM__]+\s*[i]+(\.|\))\s*\u00a0+/,
+		'I_bullet_type' : /^[__MCE_ITEM__]+\s*[I]+(\.|\))\s*\u00a0+/,
+		'one_bullet_type' : /^[__MCE_ITEM__]+\s*\d+(\.|\))\s*\u00a0+/,
+		'a_bullet_type' : /^[__MCE_ITEM__]+\s*[a-z]+(\.|\))\s*\u00a0+/,
+		'A_bullet_type' : /^[__MCE_ITEM__]+\s*[A-Z]+(\.|\))\s*\u00a0+/,
+		'indent_match' : /level(\d?)/i,
+		'middot_span_match' : /^__MCE_ITEM__[\u2022\u00b7\u00a7\u00d8o\u25CF]/,
+		'middot_span_other_match' : /^__MCE_ITEM__[\s\S]*\w+\.(&nbsp;|\u00a0)*\s*/,
+		'middot_match' : /^[\u2022\u00b7\u00a7\u00d8o\u25CF]\s*(&nbsp;|\u00a0)+\s*/,
+		'middot_other_match' : /^[\s|&nbsp;]*\w+[\.|\)](&nbsp;|\u00a0)+\s*/
+	};
+
+	pasteutils.strings = {
+		'nbsp' : '\u00a0',
+		'strike_through' : '<$1strike>',
+		'bad_attrs_marker' : '$1',
+		'list_item_marker' : '$&__MCE_ITEM__',
+		'mso_list_marker' : '$1__MCE_ITEM__',
+		'mso_symbol_item_marker' : '$1__MCE_ITEM__'
+	};
 
 	pasteutils.cleanHTML = function cleanHTML(text) {
-		var container = document.createElement('div');
-		container.innerHTML = text;
-		$(container).addClass('__paste__container__');
+		var container = $('<div>');
+		container.html(text);
 		this._preprocess(container);
 		this._postprocess(container);
-		return container.innerHTML;
+		return container.html();
 	};
 
 	pasteutils._preprocess = function _preprocess(container) {
 
-		var h = container.innerHTML, len, stripClass;
+		var h = container.html(), len;
 
 		// Detect Word content and process it more aggressive
-		if (/class="?Mso|style="[^"]*\bmso-|w:WordDocument/i.test(h)) {
-			// Process away some basic content
+		if (pasteutils.patterns.word_content.test(h)) {
 			h = this._process([
-				/^\s*(&nbsp;)+/gi,				// &nbsp; entities at the start of contents
-				/(&nbsp;|<br[^>]*>)+\s*$/gi,		// &nbsp; entities at the end of contents
+				pasteutils.patterns.beginning_nbsps,
+				pasteutils.patterns.ending_nbsps
 			], h);
 
-			h = this._process([[new RegExp(String.fromCharCode(194), 'ig'), '']], h);
+			h = this._process([[pasteutils.patterns.scrub_A_char, '']], h);
 
 			h = this._process([
-				[/<!--\[if !supportLists\]-->/gi, '$&__MCE_ITEM__'],					// Convert supportLists to a list item marker
-				[/(<span[^>]+(?:mso-list:|:\s*symbol)[^>]+>)/gi, '$1__MCE_ITEM__'],		// Convert mso-list and symbol spans to item markers
-				[/(<p[^>]+(?:MsoListParagraph)[^>]+>)/gi, '$1__MCE_ITEM__']				// Convert mso-list and symbol paragraphs to item markers (FF)
+				[pasteutils.patterns.support_list_marker, pasteutils.strings.list_item_marker],
+				[pasteutils.patterns.mso_list_marker, pasteutils.strings.mso_list_marker],
+				[pasteutils.patterns.mso_list_symbol_marker, pasteutils.strings.mso_symbol_item_marker]
 			], h);
 
 			h = this._process([
-				// Word comments like conditional comments etc
-				/<!--[\s\S]+?-->/gi,
-
+				pasteutils.patterns.conditional_comments,
 				// Remove comments, scripts (e.g., msoShowComment), XML tag, VML content, MS Office namespaced tags, and a few other tags
-				/<(!|script[^>]*>.*?<\/script(?=[>\s])|\/?(\?xml(:\w+)?|img|meta|link|style|\w:\w+)(?=[\s\/>]))[^>]*>/gi,
-
-				// Convert <s> into <strike> for line-though
-				[/<(\/?)s>/gi, "<$1strike>"],
-
-				// Replace nsbp entites to char since it's easier to handle
-				[/&nbsp;/gi, "\u00a0"]
+				pasteutils.patterns.other_comments,
+				[pasteutils.patterns.ms_strike, pasteutils.strings.strike_through],
+				[pasteutils.patterns.nbsp_marker, pasteutils.strings.nbsp]
 			], h);
 
-			// Remove bad attributes, with or without quotes, ensuring that attribute text is really inside a tag.
-			// If JavaScript had a RegExp look-behind, we could have integrated this with the last process() array and got rid of the loop. But alas, it does not, so we cannot.
-			do {
-				len = h.length;
-				h = h.replace(/(<[a-z][^>]*\s)(?:id|name|language|on\w+|\w+:\w+)=(?:"[^"]*"|\w+)\s?/gi, "$1");
-			} while (len != h.length);
+			while (pasteutils.patterns.bad_attrs.test(h))
+			  h = this._process([pasteutils.patterns.bad_attrs, '$1'], h);
 
-			h = h.replace(/<\/?span[^>]*>/gi, "");
+			h = h.replace(pasteutils.patterns.span_marker, "");
 		}
 
 		h = this._process([
 			// Copy paste from Java like Open Office will produce this junk on FF
-			[/Version:[\d.]+\nStartHTML:\d+\nEndHTML:\d+\nStartFragment:\d+\nEndFragment:\d+/gi, '']
+			[pasteutils.patterns.open_office_scrub, '']
 		], h);
 
-		// Class attribute options are: leave all as-is ("none"), remove all ("all"), or remove only those starting with mso ("mso").
-		// Note:-  paste_strip_class_attributes: "none", verify_css_classes: true is also a good variation.
-		this.stripClass = 'mso';
+		h = h.replace(pasteutils.patterns.span_marker, "");
 
-		if (stripClass !== "none") {
-			h = h.replace(/ class="([^"]+)"/gi, this._removeClasses.bind(this));
-			h = h.replace(/ class=([\-\w]+)/gi, this._removeClasses.bind(this));
-		}
+		container.html(h);
+	};
 
-		h = h.replace(/<\/?span[^>]*>/gi, "");
-
-		container.innerHTML = h;
+	pasteutils._process = function _process(items, h) {
+		items.forEach(function(v) {
+			h = h.replace(v[0], v[1] || '');
+		});
+		return h;
 	};
 
 	pasteutils._postprocess = function _postprocess(container) {
-		this.styleProps = 'none';
-
 		// Remove named anchors or TOC links
-		$(container).find('a').each(function(a) {
+		container.find('a').each(function(a) {
 			if (!a.href || a.href.indexOf('#_Toc') != -1)
 				this._remove(a, 1);
 		});
@@ -106,177 +126,128 @@
 
 	pasteutils._convertLists = function _convertLists(container) {
 		var html;
-		var lastMargin = lastType = levels = listElm = null;
+		var lastLevel = lastType = listElm = null;
 
 		// Convert middot lists into real semantic lists
-		$(container).find('p').each(this._parseParagraph.bind(this));
+		container.find('p').each(this._parseParagraph.bind(this));
 
 		// Remove any left over makers
-		html = container.innerHTML;
+		html = container.html();
 		if (html.indexOf('__MCE_ITEM__') != -1)
-			container.innerHTML = html.replace(/__MCE_ITEM__/g, '');
+			container.html(html.replace(/__MCE_ITEM__/g, ''));
 	};
 
 	pasteutils._parseParagraph = function _parseParagraph(index, p) {
-		var sib, val = '', type, html, idx, parents, margin, ol_type;
-		var container = $(p).closest('.__paste__container__');
+		var val = '', type, html, level, ol_type;
+		var container = $(p).parents().last();
 		lastType = lastType || [];
 
-		// Get text node value at beginning of paragraph
-		for (sib = p.firstChild; sib && sib.nodeType == 3; sib = sib.nextSibling)
-			val += sib.nodeValue;
+		val = $(p).text().replace(pasteutils.patterns.nbsp_marker, pasteutils.strings.nbsp);
 
-		val = p.innerHTML.replace(/<\/?\w+[^>]*>/gi, '').replace(/&nbsp;/g, '\u00a0');
-
-		// Detect unordered lists look for bullets
-		if (/^(__MCE_ITEM__)+[\u2022\u00b7\u00a7\u00d8o\u25CF]\s*\u00a0*/.test(val))
+		if (pasteutils.patterns.ul_match.test(val))
 			type = 'ul';
 
-		// Detect ordered lists 1., a. or ixv.
-		if (/^[__MCE_ITEM__]+\s*\w+(\.|\))\s*\u00a0+/.test(val)) {
+		if (pasteutils.patterns.ol_match.test(val)) {
 			type = 'ol';
-			if (/^[__MCE_ITEM__]+\s*[i]+(\.|\))\s*\u00a0+/.test(val))
-				ol_type = 'i';
-			else if (/^[__MCE_ITEM__]+\s*[I]+(\.|\))\s*\u00a0+/.test(val))
-				ol_type = 'I';
-			else if (/^[__MCE_ITEM__]+\s*\d+(\.|\))\s*\u00a0+/.test(val))
-				ol_type = '1';
-			else if (/^[__MCE_ITEM__]+\s*[a-z]+(\.|\))\s*\u00a0+/.test(val))
-				ol_type = 'a';
-			else if (/^[__MCE_ITEM__]+\s*[A-Z]+(\.|\))\s*\u00a0+/.test(val))
-				ol_type = 'A';
+			ol_type = this._getOlType(val);
 		}
 
-		// Check if node value matches the list pattern: o&nbsp;&nbsp;
 		if (type) {
-			var style_attr = $(p).attr('style');
-			var level_matches = /level(\d?)/i.exec(style_attr);
-			margin = parseFloat(level_matches[1] || 0);
+			level = this._getLevel(p);
 
-			if (!listElm || type != lastType[margin] && margin == lastMargin) {
-				listElm = $('<' + type + '>');
-				if (ol_type) listElm.attr('type', ol_type);
+			if (this._isNewList(listElm, lastType, type, level)) {
+				listElm = this._createNewList(type, ol_type);
 				listElm.insertBefore(p);
 			}
-			else {
-				// Nested list element
-				if (margin > lastMargin) {
-					var child_menu = $('<' + type + '>');
-					if (ol_type) child_menu.attr('type', ol_type);
-					$(listElm).find('li:last').append(child_menu);
-					listElm = child_menu;
-				}
-				else if (margin < lastMargin) {
-					// if current element is one level in
-					while (margin < lastMargin && lastMargin > 0) {
-						var closest_li = $(listElm).closest('li');
-						if (closest_li) {
-							listElm = $(closest_li).closest(lastType[margin]) || container;
-							lastType.pop();
-						}
-						lastMargin--;							
+			else if (this._isDeeper(level, lastLevel)) {
+				var child_menu = this._createNewList(type, ol_type);
+				listElm.find('li:last').append(child_menu);
+				listElm = child_menu;
+			}
+			else if (this._isShallower(level, lastLevel)) {
+				var num_levels_to_pop = lastLevel - level;
+				_.range(num_levels_to_pop).forEach(function() {
+				  var closest_li = $(listElm).closest('li');
+					if (closest_li) {
+						listElm = $(closest_li).closest(lastType[level - 1]) || container;
+						lastType.pop();
 					}
-				}
+				});
 			}
 
 			// Remove middot or number spans if they exists
-			$(container).find('span').each(function(span) {
-				var html = span.innerHTML.replace(/<\/?\w+[^>]*>/gi, '');
-
-				// Remove span with the middot or the number
-				if (type == 'ul' && /^__MCE_ITEM__[\u2022\u00b7\u00a7\u00d8o\u25CF]/.test(html))
-					span.remove();
-				else if (/^__MCE_ITEM__[\s\S]*\w+\.(&nbsp;|\u00a0)*\s*/.test(html))
-					span.remove();
+			container.find('span').each(function(span) {
+				this._removeMiddotSpan(span, type);
 			});
 
-			html = p.innerHTML;
-
-			// Remove middot/list items
-			if (type == 'ul')
-				html = p.innerHTML.replace(/__MCE_ITEM__/g, '').replace(/^[\u2022\u00b7\u00a7\u00d8o\u25CF]\s*(&nbsp;|\u00a0)+\s*/, '');
-			else
-				html = p.innerHTML.replace(/__MCE_ITEM__/g, '').replace(/^[\s|&nbsp;]*\w+[\.|\)](&nbsp;|\u00a0)+\s*/, '');
-
-			// Create li and add paragraph data into the new li
-			var li = $('<li>');
-			li.html(html);
-			li = $(listElm).append(li);
+			html = $(p).html();
+			html = this._paragraphMiddotFilter(type, p);
+			$(listElm).append($('<li>').html(html));
 			$(p).remove();
 
-			lastMargin = margin;
-			lastType[margin] = type;
+			lastLevel = level;
+			lastType[level - 1] = type;
 		}
 		else {
-			listElm = lastMargin = 0;
+			listElm = lastLevel = 0;
 		}
 	};
 
-	pasteutils._process = function _process(items, h) {
-		items.forEach(function(v) {
-			// Remove or replace
-			if (v.constructor == RegExp)
-				h = h.replace(v, '');
-			else
-				h = h.replace(v[0], v[1]);
-		});
-		return h;
+	pasteutils._getLevel = function _getLevel(p) {
+		var style_attr = $(p).attr('style');
+		var level_matches = pasteutils.patterns.indent_match.exec(style_attr);
+		return parseFloat(level_matches[1] || 0);
 	};
 
-	pasteutils._grep = function _grep(a, f) {
-		var o = [];
-
-		a.forEach(function(v) {
-			if (!f || f(v))
-				o.push(v);
-		});
-
-		return o;
-	};
-		
-	pasteutils._explode = function _explode(s, d) {
-		if (!s || this._is(s, 'array')) {
-			return s;
-		}
-
-		return this._map(s.split(d || ','), this._trim);
-	};
-		
-	pasteutils._trim = function _trim(s) {
-		return (s ? '' + s : '').replace(/^\s*|\s*$/g, '');
+	pasteutils._isDeeper = function _isDeeper(level, lastLevel) {
+		return level > lastLevel;
 	};
 
-	pasteutils._is = function _is(o, t) {
-		if (!t)
-			return o;
-
-		if (t == 'array' && this._isArray(o))
-			return true;
-
-		return typeof(o) == t;
+	pasteutils._isShallower = function _isShallower(level, lastLevel) {
+		return level < lastLevel;
 	};
 
-	pasteutils._isArray = function _isArray(obj) {
-		return Object.prototype.toString.call(obj) === "[object Array]";
+	pasteutils._getOlType = function _getOlType(val) {
+		var ol_type;
+		if (pasteutils.patterns.i_bullet_type.test(val))
+			ol_type = 'i';
+		else if (pasteutils.patterns.I_bullet_type.test(val))
+			ol_type = 'I';
+		else if (pasteutils.patterns.one_bullet_type.test(val))
+			ol_type = '1';
+		else if (pasteutils.patterns.a_bullet_type.test(val))
+			ol_type = 'a';
+		else if (pasteutils.patterns.A_bullet_type.test(val))
+			ol_type = 'A';
+		return ol_type;
 	};
 
-	pasteutils._removeClasses = function _removeClasses(match, g1) {
-		var cls = this._grep(this._explode(g1.replace(/^(["'])(.*)\1$/, "$2"), " "),
-			function(v) {
-				return (/^(?!mso)/i.test(v));
-			}
-		);
-
-		return cls.length ? ' class="' + cls.join(" ") + '"' : '';
+	pasteutils._isNewList = function _isNewList(listElm, lastType, type, level) {
+		return !listElm || type != lastType[level - 1] && level == lastLevel
 	};
 
-	pasteutils._map = function _map(a, f) {
-		var o = [];
-
-		a.forEach(function(v) { o.push(f(v));});
-
-		return o;
+	pasteutils._createNewList = function _createNewList(type, ol_type) {
+		var list = $('<' + type + '>');
+		if (ol_type) list.attr('type', ol_type);
+		return list;
 	};
+
+	pasteutils._removeMiddotSpan = function _removeMiddotSpan(span, type) {
+		var html = $(span).text();
+
+		// Remove span with the middot or the number
+		if (type == 'ul' && pasteutils.patterns.middot_span_match.test(html))
+			span.remove();
+		else if (pasteutils.patterns.middot_span_other_match.test(html))
+			span.remove();
+	};
+
+	pasteutils._paragraphMiddotFilter = function _paragraphMiddotFilter(type, p) {
+		if (type == 'ul')
+			return p.innerHTML.replace(/__MCE_ITEM__/g, '').replace(pasteutils.patterns.middot_match, '');
+		else
+			return p.innerHTML.replace(/__MCE_ITEM__/g, '').replace(pasteutils.patterns.middot_other_match, '');
+	}
 
 	pasteutils._remove = function _remove(node, keep_children) {
 		var child, parent = node.parentNode;
